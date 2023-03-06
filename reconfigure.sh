4a01fa525b563a4f92689d7db63ba1c740dd7b11
Christian Freitag <cfreitag.cf@googlemail.com>
2023-03-06 21:23:42 +0100

    - Added patch for automake 1.15.1 adding bolthur - Ported patch for newlib 4.3.0 and updated copyright note - Adjusted gcc patch to get g++ to work - Updated various outdated software - Replaced binutils 2.39 patch by 2.40 - Replaced dtc 1.6.1 patch by 1.7.0 - Replaced gdb 12.1 patch by 13.1

--- reconfigure.sh
+++ reconfigure.sh
@@ -4,16 +4,16 @@ set -x
 export DIR=$1
 
 # tool prefix
-export TOOL_PREFIX="/opt/bolthur/build/tmp/newlib-build"
-#export TOOL_PREFIX="/opt/bolthur/build/tmp/gcc-build"
+#export TOOL_PREFIX="/opt/bolthur/build/tmp/newlib-build"
+export TOOL_PREFIX="/opt/bolthur/build/tmp/gcc-build"
 # Extend path for sub script calls
 export PATH="$TOOL_PREFIX/bin:$PATH"
 
-# switch to source directory
+## switch to source directory
 cd "$DIR"
 echo "$DIR"
 # reconfigure
-autoconf
+autoreconf -ivf
 # check for error
 if [ $? -ne 0 ]; then
   exit 1
@@ -23,8 +23,7 @@ fi
 cd "$DIR/newlib"
 echo "$DIR/newlib"
 # reconfigure
-autoconf
-automake --cygnus Makefile
+autoreconf -ivf
 # check for error
 if [ $? -ne 0 ]; then
   exit 1
@@ -34,71 +33,93 @@ fi
 cd "$DIR/libgloss"
 echo "$DIR/libgloss"
 # reconfigure
-autoconf
+autoreconf -ivf
 # check for error
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# switch to source directory
-cd "$DIR/newlib/libm"
-# reconfigure
-autoreconf
-# check for error
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-# switch to source directory
-cd "$DIR/newlib/libc"
-# reconfigure
-autoreconf
-# check for error
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-# switch to source directory
-cd "$DIR/newlib/libm/machine/arm"
-# reconfigure
-autoreconf
-# check for error
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-# switch to source directory
-cd "$DIR/newlib/libc/machine/arm"
-# reconfigure
-autoreconf
-# check for error
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-# switch to source directory
-cd "$DIR/newlib/libc/sys"
-# reconfigure
-autoreconf
-# check for error
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-# switch to source directory
-cd "$DIR/newlib/libc/sys/bolthur"
-# reconfigure
-autoreconf
-# check for error
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-# switch to source directory
-cd "$DIR/libgloss/arm"
-# reconfigure
-autoreconf
-# check for error
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+## switch to source directory
+#cd "$DIR/newlib"
+#echo "$DIR/newlib"
+## reconfigure
+#autoconf
+#automake --cygnus Makefile
+## check for error
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## switch to source directory
+#cd "$DIR/libgloss"
+#echo "$DIR/libgloss"
+## reconfigure
+#autoconf
+## check for error
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## switch to source directory
+#cd "$DIR/newlib/libm"
+## reconfigure
+#autoreconf
+## check for error
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## switch to source directory
+#cd "$DIR/newlib/libc"
+## reconfigure
+#autoreconf
+## check for error
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## switch to source directory
+#cd "$DIR/newlib/libm/machine/arm"
+## reconfigure
+#autoreconf
+## check for error
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## switch to source directory
+#cd "$DIR/newlib/libc/machine/arm"
+## reconfigure
+#autoreconf
+## check for error
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## switch to source directory
+#cd "$DIR/newlib/libc/sys"
+## reconfigure
+#autoreconf
+## check for error
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## switch to source directory
+#cd "$DIR/newlib/libc/sys/bolthur"
+## reconfigure
+#autoreconf
+## check for error
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+## switch to source directory
+#cd "$DIR/libgloss/arm"
+## reconfigure
+#autoreconf
+## check for error
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
